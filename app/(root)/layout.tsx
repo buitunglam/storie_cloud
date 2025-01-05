1c95ce8b2afd79f1fee127f4c28a028515b8c3cf
buitunglam <tunglam.xm94@gmail.com>
2025-01-05 17:03:42 +0700

    add force dynamic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,21 +6,26 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 
+export const dynamic = "force-dynamic";
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const currentUser = await getCurrentUser();
-  console.log("currentUser---", currentUser);
-  if (!currentUser) return redirect("sign-in");
-  return (
-    <main className="flex h-screen">
-      <Sidebar {...currentUser} />
-      <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation {...currentUser} />
-        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
-        <div className="main-content">{children}</div>
-      </section>
-      <Toaster />
-    </main>
-  );
+    const currentUser = await getCurrentUser();
+    console.log("currentUser---", currentUser);
+    if (!currentUser) return redirect("sign-in");
+    return (
+        <main className="flex h-screen">
+            <Sidebar {...currentUser} />
+            <section className="flex h-full flex-1 flex-col">
+                <MobileNavigation {...currentUser} />
+                <Header
+                    userId={currentUser.$id}
+                    accountId={currentUser.accountId}
+                />
+                <div className="main-content">{children}</div>
+            </section>
+            <Toaster />
+        </main>
+    );
 };
 
 export default Layout;
